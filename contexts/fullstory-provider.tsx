0bd970a4bb544f2c921069401fd2edd3a90add2b
Kyle Ledbetter <kyle@kyleledbetter.com>
2024-10-22 10:36:05 -0500

    chore(): fullstory conditional
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- contexts/fullstory-provider.tsx
+++ contexts/fullstory-provider.tsx
@@ -9,8 +9,13 @@ interface FullstoryProviderProps {
 
 function FullstoryProvider({ children }: FullstoryProviderProps) {
   useEffect(() => {
-    fullstory.init({ orgId: process.env.NEXT_PUBLIC_FULLSTORY_ORG })
-    ;(window as any)._fs_run_in_iframe = true
+    const orgId = process.env.NEXT_PUBLIC_FULLSTORY_ORG
+    if (orgId) {
+      fullstory.init({ orgId })
+      ;(window as any)._fs_run_in_iframe = true
+    } else {
+      console.warn("Fullstory orgId not found. Skipping initialization.")
+    }
   }, [])
 
   return <>{children}</>
