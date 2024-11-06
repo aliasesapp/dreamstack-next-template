6b532f0bb2dc7d3676df84d8bb63988423d73e5f
Kyle Ledbetter <kyle@kyleledbetter.com>
2024-11-06 09:58:26 -0600

    fix(): remove themeColor from metadata
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,6 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
   icons: {
     icon: [
       { url: `${iconUrl}/favicon-16x16.png`, sizes: "16x16" },
