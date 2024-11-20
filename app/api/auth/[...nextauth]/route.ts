a529c7548caf20409d5fa364b98cfc57cd2a9009
Kyle Ledbetter <kyle@kyleledbetter.com>
2024-11-20 12:01:20 -0600

    fix(auth): no error when no provider
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { handlers } from "lib/auth"
-export const { GET, POST } = handlers
+import { handlers, isAuthEnabled } from "lib/auth"
+
+export const { GET, POST } = isAuthEnabled() ? handlers : {
+  GET: () => new Response(null, { status: 200 }),
+  POST: () => new Response(null, { status: 200 })
+}
